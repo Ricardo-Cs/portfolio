53d1262ce69882ff0d25f7f89ec94f45c21afcd9
Ricardo Costa da Silva <rcsilva2405@gmail.com>
2023-08-07 18:03:38 -0500

    Home section finished

--- js/script.js
+++ js/script.js
@@ -13,3 +13,39 @@ themeButton.addEventListener('click', () => {
     }
     
 });
+
+// Type effect
+const textElement = document.getElementById('author-dev-area');
+const text = 'Desenvolvedor FullStack Júnior'; // The text you want to type out
+const typingSpeed = 100; // Adjust typing speed in milliseconds
+const eraseSpeed = 50; // Adjust erasing speed in milliseconds
+
+function typeAndErase() {
+    textElement.textContent = ''; // Clear the text element before typing again
+    
+    function type() {
+        if (charIndex < text.length) {
+            textElement.textContent += text.charAt(charIndex);
+            charIndex++;
+            setTimeout(type, typingSpeed);
+        } else {
+            setTimeout(erase, 1000); // Wait for a second before erasing
+        }
+    }
+
+    function erase() {
+        if (charIndex > 0) {
+            textElement.textContent = textElement.textContent.slice(0, -1);
+            charIndex--;
+            setTimeout(erase, eraseSpeed);
+        } else {
+            charIndex = 0; // Reset charIndex to start typing again
+            setTimeout(type, typingSpeed);
+        }
+    }
+
+    let charIndex = 0;
+    type(); // Start typing
+}
+
+typeAndErase(); // Call the function to initiate the typing and erasing loop
